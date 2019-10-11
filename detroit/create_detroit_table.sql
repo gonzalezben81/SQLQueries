73a0ce91bea8a6db154cc9e93c9dc253597b4e08
gonzalezben81 <gonzalezben81@gmail.com>
2019-10-11 22:08:21 +0000

    Initial commit.

---
+++ detroit/create_detroit_table.sql
@@ -0,0 +1,18 @@
+-- !preview conn=DBI::dbConnect(RSQLite::SQLite())
+
+CREATE TABLE public.detroit (
+    "FTP" double precision,
+    "UEMP" double precision,
+    "MAN" double precision,
+    "LIC" double precision,
+    "GR" double precision,
+    "CLEAR" double precision,
+    "WM" integer,
+    "NMAN" double precision,
+    "GOV" double precision,
+    "HE" double precision,
+    "WE" double precision,
+    "HOM" double precision,
+    "ACC" double precision,
+    "ASR" double precision
+);
